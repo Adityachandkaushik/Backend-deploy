c49d379a7bdc408d6e9d55772548a530b05c3a0e
Aditya kaushik <165358967+Adityachandkaushik@users.noreply.github.com>
2026-09-23 11:06:24 +0530

    Fix formatting in analytics.controller.js

--- src/modules/analytics/analytics.controller.js
+++ src/modules/analytics/analytics.controller.js
@@ -3,7 +3,7 @@ const service = require("./analytics.service");
 
 exports.report = async (req, res, next) => {
   try {
-    const data = await service.getReport();
+    const data = await service.getReport();   
     res.json(data);
   } catch (e) {
     next(e);
